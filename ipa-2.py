ca8da0ab050e487152d13c888d69bda4cbdce835
Stephen Daeniel Mansueto <stephen@Stephens-MacBook-Air.local>
2023-06-27 20:14:49 +0800

    resubmission for ipa 2 and 3

--- ipa-2.py
+++ ipa-2.py
@@ -39,8 +39,9 @@ def shift_letter(letter, shift):
     # Stay within the function. Only use the parameters as input. The function should return your answer.
     alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     if letter not in alphabet:
-        acc+=letter
-    new_letter = alphabet[(alphabet.index(letter) + shift) % 26]
+        new_letter = letter
+    else:
+        new_letter = alphabet[(alphabet.index(letter) + shift) % 26]
     return new_letter
     
 
